508e24b9bb428250c71066d69c79ca011a4f4903
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 17:18:48 +0530

    Add src/utils/cloudinary.js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto'
+    });
+    // file has been uploaded successfully
+    // console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+const deleteFromCloudinary = async (publicId, resourceType = 'image') => {
+  try {
+    if (!publicId) return null;
+    const result = await cloudinary.uploader.destroy(publicId, { resource_type: resourceType });
+    return result;
+  } catch (error) {
+    console.log('Error deleting from Cloudinary: ', error);
+    return null;
+  }
+}
+
+export { uploadOnCloudinary, deleteFromCloudinary };
