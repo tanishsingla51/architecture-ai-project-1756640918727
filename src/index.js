87e08ddda16d6c2013749f4178918b90fe7e7661
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 17:18:43 +0530

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { app } from './app.js';
+
+dotenv.config({
+  path: './.env'
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`\n⚙️  Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
