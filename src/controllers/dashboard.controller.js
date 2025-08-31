709dba3b70f0ec7cc55ee25dc710aba0f8305dd9
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 17:19:06 +0530

    Add src/controllers/dashboard.controller.js

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+import {Video} from "../models/video.model.js"
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    return res.status(200).json(new ApiResponse(200, {}, "Channel stats fetched"));
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+    return res.status(200).json(new ApiResponse(200, [], "Channel videos fetched"));
+})
+
+export {
+    getChannelStats,
+    getChannelVideos
+    }
